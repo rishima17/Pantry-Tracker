dacb050284948f2f1781b3d180a34efb23d7a3f9
Rishima <rishima1711@gmail.com>
2025-11-15 01:03:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,20 +7,29 @@ import NotFound from "./pages/NotFound";
 import ExpiringSoon from "./pages/ExpiringSoon";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Footer from "./components/Footer";
+import AddItem from "./pages/AddItem";
+import About from "./pages/About";
+import Item from "./components/Item";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/addItem" element={<AddItem />} />
+        <Route path="/pantry" element={<Item />} />
 
-        <Route path="/" element={<Home />} />
         <Route path="/expiringSoon" element={<ExpiringSoon />} />
+        
 
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
